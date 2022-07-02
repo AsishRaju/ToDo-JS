268c0ae4f9c4665ed2db7ac989434209da26211d
VaradAC <46750431+VaradAC@users.noreply.github.com>
2022-07-02 10:34:25 +0530

    Trying to add edit-task-feature

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const addRef = document.querySelector('.add')
 const listRef=document.querySelector('.todos')
 const searchRef=document.querySelector('.search input')
 const submitRef=document.querySelector('.submit')
+const editRef=document.querySelector('.edit')
+
+//const saveInd = document.getElementById("saveIndex");
+const form = document.querySelector(".inputform")
+let index = 0;
 var todosList =[]
 const generateTemplate= todo=>{
 
-    const htmlTemplate=`<li class="list-group-item d-flex justify-content-between align-items-center">
+    const htmlTemplate=`<li class="list-group-item d-flex justify-content-between align-items-center individual-item">
     <span>${todo}</span>
-    <i class="fas fa-times delete" style="color:red;"></i>
+    <span>
+    <i class="fas fa-edit" style="color:green"></i>
+    <i class="fas fa-times delete" style="color:red"></i>
+    <span>
     </li>`
 listRef.innerHTML=htmlTemplate+listRef.innerHTML
 }
@@ -20,7 +28,7 @@ const filterTodo=(term)=>{
     Array.from(listRef.children)
     .filter((todo)=> todo.textContent.toLowerCase().includes(term.toLowerCase()))
     .forEach((todo) => todo.classList.remove('filtered'))
-    
+
 }
 
 searchRef.addEventListener('keyup',()=>{
@@ -32,13 +40,25 @@ searchRef.addEventListener('keyup',()=>{
 listRef.addEventListener('click',(e)=>{
     if(e.target.classList.contains('delete'))
     {
-        e.target.parentElement.remove()
-        const toDelete=e.target.parentElement.textContent.trim()
+        e.target.parentElement.parentElement.remove()
+        const toDelete=e.target.parentElement.parentElement.textContent.trim()
         todosList = todosList.filter(item =>{
             return item !== toDelete
         })
         localStorage.clear()
         localStorage.setItem('prevTodos',JSON.stringify(todosList))
+        console.log(localStorage.getItem('prevTodos'));
+
+    }
+
+    if(e.target.classList.contains('fa-edit'))
+    {
+        form.value = e.target.parentElement.previousElementSibling.innerHTML;
+
+        submitRef.style.display ="none";
+        editRef.style.display = "block";
+
+
     }
 })
 
@@ -50,16 +70,44 @@ const add=()=>{
     const newTodo=addRef.inputField.value.trim()
     todosList.push(newTodo)
     localStorage.setItem('prevTodos',JSON.stringify(todosList))
+    console.log(todosList);
     if(newTodo !== "")
     {
         generateTemplate(newTodo)
-        
+
     }
     addRef.reset()
+    for(i=1;i<=todosList.length;i++){
+    console.log(document.querySelector(`.individual-item:nth-child(${i})`).firstElementChild.innerHTML)}
 }
 
 submitRef.addEventListener('click',(e)=>{
     add();
+})
+
+
+//logic to edit tasks in to-do list
+const edit = (e) =>{
+    let x = 0;
+    for(let i=1; i<=todosList.length;i++){
+        if(todosList[i-1] === form.value){
+
+            x=i;
+            console.log(x);
+            console.log(document.querySelector(`.individual-item:nth-child(${x})`).firstElementChild);
+        }
+    }
+    todosList[x-1] === form.value;
+    console.log(form.value);
+    console.log(x);
+    //document.querySelector(`.individual-item:nth-child(${x})`).firstElementChild.innerHTML = `${form.value}`;
+
+}
+
+//Event listener on edit button
+editRef.addEventListener('click',(e)=>{
+    edit(e);
+    reset();
 })
 
 if(localStorage.getItem('prevTodos'))
@@ -69,3 +117,10 @@ if(localStorage.getItem('prevTodos'))
         generateTemplate(Element)
     })
 }
+
+const reset = () => {
+    submitRef.style.display ="block";
+    editRef.style.display = "none";
+}
+
+
